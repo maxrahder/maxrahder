6683fd445cbeaeb3213c5c8d8dfa95d47b32ca96
Max Rahder <max@rahder.com>
2017-01-19 11:29:50 -0600

    self

---
+++ workspace/packages/local/Deck/classic/src/view/topics/TopicsMethods.js
@@ -0,0 +1,26 @@
+Ext.define('Deck.view.topics.TopicsMethods', {
+
+    goToPage: function(value, forward) {
+        var me = this;
+        forward = Ext.isDefined(forward) ? !!forward : true;
+        if (value) {
+            // Pressing enter searches forward, shift-enter searhces backwards.
+            var vm = me.getViewModel();
+            var store = vm.get('topics');
+            if (!store) {
+                return;
+            }
+            var node = vm.get('node');
+            var found = store.findNode(node, value, forward);
+            if (found) {
+                me.getView().setSelection(found);
+                var parent = found.parentNode;
+                while (parent) {
+                    parent.expand();
+                    parent = parent.parentNode;
+                }
+            }
+        }
+    }
+
+});
